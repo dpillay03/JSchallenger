70c718fe298c1709683ba6700807b52b7573cecd
dpillay03 <dpillay03@gmail.com>
2022-03-01 21:21:27 -1000

    added javascript basics file with first three challenges

---
+++ javascript_basics/index.js
@@ -0,0 +1,22 @@
+// Write a function that takes two numbers (a and b) as argument
+// Sum a and b
+// Return the result
+function myFunction1(a, b) {
+  var sum = a + b;
+  return sum;
+}
+
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
+function myFunction2(a, b) {
+  if (a !== b) {
+    return false;
+  }
+  return true;
+}
+
+// Write a function that takes a value as argument
+// Return the type of the value
+function myFunction3(a) {
+  return typeof a;
+}
