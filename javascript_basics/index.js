ca1b6bfc534133d840ba493a59008cfaec170dd9
dpillay03 <dpillay03@gmail.com>
2022-03-02 21:07:45 -1000

    updated index.js with Get nth character of string challenge

--- javascript_basics/index.js
+++ javascript_basics/index.js
@@ -20,3 +20,9 @@ function myFunction2(a, b) {
 function myFunction3(a) {
   return typeof a;
 }
+
+// Write a function that takes a string (a) and a number (n) as argument
+// Return the nth character of 'a'
+function myFunction4(a, n) {
+  return a[n - 1];
+}
